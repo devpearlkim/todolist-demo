7020df125dc3859cc7f35bbb0bf2c6a4afb90a16
JINJU <devpearlkim@gmail.com>
2024-10-18 22:31:11 +0900

    Update MongoDB connection

--- app.js
+++ app.js
@@ -2,15 +2,18 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const indexRouter = require('./routes/index');
+const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
+app.use(cors());
 app.use(bodyParser.json());
 app.use('/api', indexRouter);
 
-const mongoURI = `mongodb://localhost:27017/todo-demo`;
+const mongoURI = process.env.MONGODB_URI;
 
 mongoose
-  .connect(mongoURI, { useNewUrlParser: true })
+  .connect(mongoURI)
   .then(() => console.log('mongoose connected'))
   .catch((error) => console.log('DB connection fail', error));
 
